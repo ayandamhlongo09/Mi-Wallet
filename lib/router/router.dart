66c77d85960cb71609246d2b1314223fa0fb818c
ayandamhlongo09 <40205374+ayandamhlongo09@users.noreply.github.com>
2024-07-29 08:03:51 +0200

    feat: Added  new page thatt allows Scanniing or manually adding of credit card details

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,10 +1,12 @@
 import 'package:go_router/go_router.dart';
 import 'package:mi_wallet/components/bottom_nav_bar.dart';
+import 'package:mi_wallet/pages/add_card_page.dart';
 import 'package:mi_wallet/pages/wallet_page.dart';
 
 class AppRoutes {
   static const String wallet = "wallet";
   static const String bottomNav = "bottomNav";
+  static const String addCard = "addCard";
 
   static GoRouter router = GoRouter(
     routes: [
@@ -17,6 +19,11 @@ class AppRoutes {
         name: wallet,
         path: "/wallet",
         builder: (context, state) => const WalletPage(),
+      ),
+      GoRoute(
+        name: addCard,
+        path: "/addCard",
+        builder: (context, state) => const AddCard(),
       ),
     ],
   );
